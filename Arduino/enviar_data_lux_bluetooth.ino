ccdb38d869d2814dc1e1c0b00c0c41108b5caeb8
Alejandro Manzanares Lemus <alexmanza2@gmail.com>
2018-09-27 15:47:22 +0200

    Añadidos codigos proyecto

---
+++ Arduino/enviar_data_lux_bluetooth.ino
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Autor: Alejandro Manzanares Lemus
+//
+// Programa que lee valores del sensor de luz y los envia a la
+// raspberry pi a traves de un modulo bluetooth 
+//
+////////////////////////////////////////////////////////////////////////////////////
+int lighPin=0; //Selecciona el pin 0 como la fuente del dato que recive
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(lighPin,INPUT);
+}
+
+void loop() {
+  int reading = analogRead(lighPin); //Reading lee valores del LightPin
+  //Serial.print('D');
+  char str_reading[12];
+  sprintf(str_reading,"%d",reading); //El dato de int se convierte a str
+  Serial.print(str_reading);
+  Serial.print('\n');
+  //delay(10); //
+}
